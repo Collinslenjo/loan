740e6045d4250c3a1d6d1794bd4a9a97b454a623
collins lenjo <collinslenjo.cl@gmail.com>
2017-04-19 21:00:12 +0300

    stima zimepotea 2

--- src/models.py
+++ src/models.py
@@ -13,6 +13,6 @@ class Loan(models.Model):
 
 class Payments(models.Model):
 	title = models.CharField(max_length=120)
-	content = models.TextField()
+	amount = models.TextField()
 	updated = models.DateTimeField(auto_now = True, auto_now_add = False)
 	timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)
