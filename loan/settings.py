e48c8db0ae06ee2e41c7e91389b128da40a9dce8
collins lenjo <collinslenjo.cl@gmail.com>
2017-04-22 13:28:27 +0300

    sato

--- loan/settings.py
+++ loan/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'src',
+    'material',
+        'material.frontend',
+        'material.admin',
 )
 
 MIDDLEWARE_CLASSES = (
